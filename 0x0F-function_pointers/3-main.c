79eb1cb98b2b82e33c5cf6138d2eb375d59f466c
brian <muriithibrian283@gmail.com>
2024-07-24 22:01:49 +0300

    Create readme file and files 0 to 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	int (*func)(int, int);
+	char a;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	a = *argv[2];
+
+	if ((a == '/' || a == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(n1, n2));
+	return (0);
+}
