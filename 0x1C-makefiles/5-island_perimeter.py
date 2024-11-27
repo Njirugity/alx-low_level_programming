c131d90d67e89dd9b20e06f915269ba7e6613e4d
brian <muriithibrian283@gmail.com>
2024-11-27 16:32:31 +0300

    Add solution to tasks 0 to 5 and advanced task 100

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin python3
+""" Defines an island perimeter calculating function"""
+
+def island_perimeter(grid):
+    """Return the perimeter of an island
+    Arg:
+        grid: A list of list of integers
+    Return:
+        perimeter: Perimeter of an island"""
+    
+    height = len(grid)
+    length = len(grid[0])
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(length):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i -1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
